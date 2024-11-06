032921eb092bf6b7a61551c403f274e72793243b
Darshan shah <darshan@mytradezone.com>
2024-11-06 13:06:39 -0500

    Added constants for bucket name and file paths

---
+++ dags/constants.py
@@ -0,0 +1,14 @@
+# Constants for Google Cloud Storage and file paths
+BUCKET_NAME = 'us-east1-climasmart-fefe9cc2-bucket'
+ENGINEERED_DAILY_DATA_PATH = 'weather_data/engineered_daily_data.csv'
+ENGINEERED_HOURLY_DATA_PATH = 'weather_data/engineered_hourly_data.csv'
+DAILY_SCHEMA_PATH = 'weather_data_validation/daily_schema.pkl'
+HOURLY_SCHEMA_PATH = 'weather_data_validation/hourly_schema.pkl'
+DAILY_STATS_PATH = 'weather_data_validation/daily_stats.pkl'
+HOURLY_STATS_PATH = 'weather_data_validation/hourly_stats.pkl'
+DAILY_DATA_PATH = 'weather_data/daily_weather_data.csv'
+HOURLY_DATA_PATH = 'weather_data/hourly_weather_data.csv'
+PREPROCESSED_DAILY_DATA_PATH = 'weather_data/preprocessed_daily_data.csv'
+PREPROCESSED_HOURLY_DATA_PATH = 'weather_data/preprocessed_hourly_data.csv'
+WEATHER_DATA_PLOTS_PATH = 'weather_data_plots/'
+
