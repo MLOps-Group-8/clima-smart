e19c1fd6cec3fe6b1693df767a336d1fd39f6cf2
keshiarun01 <keshiarun01@gmail.com>
2024-11-13 01:11:21 -0500

    Key deleted

--- dags/weather_data_collection_dag.py
+++ dags/weather_data_collection_dag.py
@@ -10,7 +10,7 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 
 
 API_URL = "https://archive-api.open-meteo.com/v1/archive"
-BUCKET_NAME = 'clima-smart-data-collection'
+BUCKET_NAME = 'us-east1-climasmart-fefe9cc2-bucket'
 
 default_args = {
     'owner': 'airflow',
@@ -24,6 +24,32 @@ default_args = {
 
 dag = DAG('weather_data_pipeline', default_args=default_args, schedule_interval=None)
 
+<<<<<<< Updated upstream
+=======
+# Define function to notify failure or sucess via an email
+def notify_success(context):
+    success_email = EmailOperator(
+        task_id='success_email',
+        to='keshiarun01@gmail.com',
+        subject='Success Notification from Airflow',
+        html_content='<p>The task succeeded.</p>',
+        dag=context['dag']
+    )
+    success_email.execute(context=context)
+
+def notify_failure(context):
+    failure_email = EmailOperator(
+        task_id='failure_email',
+        to='keshiarun01@gmail.com',
+        subject='Failure Notification from Airflow',
+        html_content='<p>The task failed.</p>',
+        dag=context['dag']
+    )
+    failure_email.execute(context=context)
+
+
+# Task to fetch and save daily weather data
+>>>>>>> Stashed changes
 def get_daily_weather_data():
     logging.info("Starting the daily weather data task.")
     client = setup_session()
@@ -68,4 +94,67 @@ visualize_and_upload_task = PythonOperator(
     dag=dag
 )
 
+<<<<<<< Updated upstream
 daily_weather_task >> hourly_weather_task >> visualize_and_upload_task
+=======
+preprocess_hourly_task = PythonOperator(
+    task_id='preprocess_hourly_data',
+    python_callable=preprocess_hourly_weather_data,
+    on_failure_callback=notify_failure,
+    dag=dag
+)
+
+feature_engineering_task = PythonOperator(
+    task_id='feature_engineering',
+    python_callable=perform_feature_engineering,
+    on_failure_callback=notify_failure,
+    dag=dag
+)
+
+eda_and_visualizations_task = PythonOperator(
+    task_id='eda_and_visualization',
+    python_callable=eda_and_visualizations,
+    on_failure_callback=notify_failure,
+    dag=dag
+)
+
+
+# Updated task for generating and saving schema and stats
+generate_and_save_schema_stats_task = PythonOperator(
+    task_id='generate_and_save_schema_stats',
+    python_callable=save_schema_and_stats,
+    op_args=['daily_schema.pkl', 'hourly_schema.pkl', 'daily_stats.pkl', 'hourly_stats.pkl'],
+    op_kwargs={
+        'bucket_name': BUCKET_NAME,
+        'destination_path': 'weather_data_validation'
+    },
+    on_failure_callback=notify_failure,
+    dag=dag
+)
+
+# Validation tasks
+validate_data_task = PythonOperator(
+    task_id='validate_weather_data',
+    python_callable=validate_daily_and_houlry_weather_data,
+    on_failure_callback=notify_failure,
+    dag=dag
+)
+
+schema_quality_test_task = PythonOperator(
+    task_id='test_data_quality_and_schema',
+    python_callable=test_weather_data_quality_and_schema,
+    on_failure_callback=notify_failure,
+    dag=dag
+)
+
+email_notification_task = EmailOperator(
+    task_id='send_email_notification',
+    to='keshiarun01@gmail.com',
+    subject='Dag Completed Successfully',
+    html_content='<p>Dag Completed</p>',
+    dag=dag,
+)
+
+# Set task dependencies
+daily_weather_task >> hourly_weather_task >> preprocess_daily_task >> preprocess_hourly_task >> feature_engineering_task >> eda_and_visualizations_task >> generate_and_save_schema_stats_task >> validate_data_task >> schema_quality_test_task >> email_notification_task
+>>>>>>> Stashed changes
